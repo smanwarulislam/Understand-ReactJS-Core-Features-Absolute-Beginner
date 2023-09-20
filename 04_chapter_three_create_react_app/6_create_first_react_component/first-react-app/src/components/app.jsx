acd10c1d7c7efc573653dce023cebc1e321f31e9
smanwarulislam <engg.anwarbd@gmail.com>
2023-09-21 03:10:39 +0600

    Added Create First React Component

---
+++ 04_chapter_three_create_react_app/6_create_first_react_component/first-react-app/src/components/app.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// class App extends React.Component {
+
+// }
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <h1>Hello React, You are Awesome! That's Great, It is Working!</h1>;
+  }
+}
+
+export default App;
