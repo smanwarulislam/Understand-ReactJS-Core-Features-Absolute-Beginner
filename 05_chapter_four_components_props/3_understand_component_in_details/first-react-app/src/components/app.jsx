f919d6305d05184c239c2b9d50b12bea884a3c86
smanwarulislam <engg.anwarbd@gmail.com>
2023-09-27 03:01:13 +0600

    Added Understand Component in Details (npm run eject)

---
+++ 05_chapter_four_components_props/3_understand_component_in_details/first-react-app/src/components/app.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+
+// class App extends React.Component {
+
+// }
+
+import React, { Component } from "react";
+
+class App extends Component {
+  // Data and Logic Layer (Optional)
+  /**
+   * 1. State
+   * 2. Variable or Properties
+   * 3. Functions or Methods
+   * 4. Lifecycle Methods
+   */
+
+  // Representation Layer (Required)
+  /**
+   * 5. JSX (JavaScript Syntax Extension and occasionally referred to as JavaScript XML)
+   */
+  render() {
+    return <h1>Hello React, You are Awesome! That's Great, It is Working!</h1>;
+  }
+}
+
+export default App;
