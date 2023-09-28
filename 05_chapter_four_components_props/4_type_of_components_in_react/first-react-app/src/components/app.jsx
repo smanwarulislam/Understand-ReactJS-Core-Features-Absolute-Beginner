21b8c84c9124c812463723debe41fde7974b8d57
smanwarulislam <engg.anwarbd@gmail.com>
2023-09-29 01:00:11 +0600

    Added Type of Components in React (npm run eject)

---
+++ 05_chapter_four_components_props/4_type_of_components_in_react/first-react-app/src/components/app.jsx
@@ -0,0 +1,33 @@
+// import React from "react";
+
+// class App extends React.Component {
+
+// }
+
+import React, { Component } from "react";
+
+class App extends Component {
+  // Data and Logic Layer (Optional)
+  /**
+   * 1. State
+   * 2. Variable or Properties
+   * 3. Functions or Methods
+   * 4. Lifecycle Methods
+   */
+
+  // Representation Layer (Required)
+  /**
+   * 5. JSX (JavaScript Syntax Extension and occasionally referred to as JavaScript XML)
+   */
+  render() {
+    return <h1>Hello React, You are Awesome! That's Great, It is Working!</h1>;
+  }
+}
+
+export default App;
+
+/**
+ * There are Two Different Ways to Create React Component
+ * 1. Class-based Component / Stateful Component / Smart Component
+ * 2. Functional Component / Stateless Component / Dumb Component
+ */
