c856fd3a6d2a35bd415e72ff94a31328d1f6bb89
smanwarulislam <engg.anwarbd@gmail.com>
2023-10-17 23:10:34 +0600

    Added Split Profile Component into Child Components (npm run eject)

---
+++ 05_chapter_four_components_props/11_split_profile_component_into_child_components/first-react-app/src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Profile from "./profile";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <h1>Create Simple Profile Apps</h1> */}
+        <Profile />
+      </div>
+    );
+  }
+}
+
+export default App;
