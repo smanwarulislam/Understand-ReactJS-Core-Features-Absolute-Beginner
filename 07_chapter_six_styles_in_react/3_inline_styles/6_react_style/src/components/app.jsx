3ae4ac65e1a0b1b85e41bfdfa411be8364a370cb
smanwarulislam <engg.anwarbd@gmail.com>
2023-11-22 23:00:27 +0600

    Added Inline Styles (npm run eject)

---
+++ 07_chapter_six_styles_in_react/3_inline_styles/6_react_style/src/components/app.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import Button from "./button";
+import BoxA from "./box_a";
+import BoxB from "./box_b";
+
+const myHeader = {
+  color: 'blue',
+  fontWeight: 300,
+  fontFamily: 'Arial',
+  fontSize: '36px'
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={myHeader}>How to Style React App</h1>
+        {/* <h1 style={myHeader}>Another Header Tag</h1> */}
+        <h1 
+          style={{
+            color: 'blue',
+            fontWeight: 300,
+            fontFamily: 'Arial',
+            fontSize: '36px'
+          }}
+        >
+          Another Header Tag
+        </h1>
+        <Button>Click Me</Button>
+        <BoxA />
+        <BoxB />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/**
+ * There are four main methods of styling the React app
+ * - Inline CSS
+ * - CSS Class
+ * - CSS Modules
+ * - JSS (Styled Components)
+ */
---
+++ 07_chapter_six_styles_in_react/3_inline_styles/6_react_style/src/components/box_a.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Button from "./button";
+
+const Box = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pellentesque risus a ullamcorper placerat.
+      </p>
+      <Button>Alert A</Button>
+    </div>
+  );
+};
+
+export default Box;
