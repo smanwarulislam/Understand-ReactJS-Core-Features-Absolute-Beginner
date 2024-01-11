6b3cee13ce0961c1fdcc13b0710bb7f819d62fc8
smanwarulislam <engg.anwarbd@gmail.com>
2024-01-11 22:01:07 +0600

    Added Game Winning Logic

---
+++ project_one_tic_tac_toe/5_game_winning_logic/10-tic-tac-toe/src/components/game.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Board from "./board";
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+class Game extends React.Component {
+  state = {
+    history: [{ squares: Array(9).fill(null) }],
+    stepNumber: 0,
+    xIsNext: true
+  };
+
+  handleClick = i => {
+    // console.log(i);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{ squares }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  };
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    console.log(winner);
+    return (
+      <div>
+        <h1>Game Component</h1>
+        <Board onClick={this.handleClick} squares={current.squares} />
+      </div>
+    );
+  }
+}
+
+export default Game;
