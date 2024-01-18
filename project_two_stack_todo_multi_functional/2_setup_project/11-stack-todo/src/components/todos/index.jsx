6d4dc834314d1cb5c008b744b636ce4e082d1726
smanwarulislam <engg.anwarbd@gmail.com>
2024-01-19 01:20:03 +0600

    Added Setup Project

---
+++ project_two_stack_todo_multi_functional/2_setup_project/11-stack-todo/src/components/todos/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Todos extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-4 text-center mb-5">Stack Todos</h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
