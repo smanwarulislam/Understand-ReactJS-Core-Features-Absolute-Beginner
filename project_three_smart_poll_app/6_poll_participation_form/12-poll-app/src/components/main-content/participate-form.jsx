d0add6ea124dec7ad6a665fd560d80fda1693795
smanwarulislam <engg.anwarbd@gmail.com>
2024-03-03 05:10:03 +0600

    Added Poll Participation Form

---
+++ project_three_smart_poll_app/6_poll_participation_form/12-poll-app/src/components/main-content/participate-form.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import { Form, FormGroup, Label, Input, FormFeedback, Button } from 'reactstrap';
+
+class ParticipationForm extends React.Component {
+  state = {
+    name: '',
+    selectedOption: '',
+    errors: {}
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { errors, isValid } = this.validate();
+
+    if (isValid) {
+      this.props.getOpinion({
+        pollId: this.props.poll.id,
+        name: this.state.name,
+        selectedOption: this.state.selectedOption
+      });
+      event.target.reset();
+      this.setState({
+        name: '',
+        selectedOption: '',
+        errors: {}
+      });
+    } else {
+      this.setState({ errors });
+    }
+  };
+
+  validate = () => {
+    const errors = {};
+
+    if (!this.state.name) {
+      errors.name = 'Please Provide A Name';
+    } else if (this.state.name.length > 20) {
+      errors.name = 'Name Too Long';
+    }
+
+    if (!this.state.selectedOption) {
+      errors.selectedOption = 'Please Select One Option';
+    }
+
+    return {
+      errors,
+      isValid: Object.keys(errors).length === 0
+    };
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <div className="d-flex">
+          <h4>Options</h4>
+          <Button 
+            color="warning" 
+            type="button" 
+            className="ms-auto" 
+            onClick={this.props.toggleModal}
+          >
+            Edit
+          </Button>
+          <Button 
+            color="danger" 
+            type="button" 
+            className="ms-2" 
+            onClick={() => this.props.deletePoll(this.props.poll.id)}
+          >
+            Delete
+          </Button>
+        </div>
+        {this.props.poll.options.map(opt => (
+          <FormGroup className="my-2" key={opt.id}>
+            <Label className="d-flex">
+              <Input 
+                type="radio"
+                id={opt.id}
+                name="selectedOption"
+                value={opt.id}
+                onChange={this.handleChange}
+                invalid={this.state.errors.selectedOption ? true : false}
+              />
+              {opt.value}
+              <span 
+                style={{
+                  background: 'green',
+                  color: 'white',
+                  padding: '5px 20px',
+                  borderRadius: '5px'
+                }}
+                className="ms-auto"
+              >
+                {opt.vote}
+              </span>
+              <span 
+                style={{
+                  background: 'orange',
+                  color: 'white',
+                  padding: '5px 20px',
+                  borderRadius: '5px'
+                }}
+                className="ms-2"
+              >
+                {this.props.poll.totalVote > 0 
+                  ? ( 
+                      (100 * opt.vote) / 
+                      this.props.poll.totalVote
+                    ).toFixed(2) 
+                  : 0}
+                %
+              </span>
+            </Label>
+          </FormGroup>
+        ))}
+        <FormGroup className="my-3">
+          <Label>Enter Your Name</Label>
+          <Input 
+            name="name"
+            placeholder="S M Anwarul Islam Raju"
+            value={this.state.value}
+            onChange={this.handleChange}
+            invalid={this.state.errors.name ? true : false}
+          />
+          {this.state.errors.name && (
+            <FormFeedback>{this.state.errors.name}</FormFeedback>
+          )}
+        </FormGroup>
+        <Button type="submit">Submit Your Opinion</Button>
+      </Form>
+    );
+  }
+}
+
+export default ParticipationForm;
